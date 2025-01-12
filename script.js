5a89ce129f802dca977671ec8fd9bf819898f856
Muhammad Usman <usmannoor758236@gmail.com>
2025-01-12 09:13:17 +0500

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,8 @@ var validPassword = "1234567";
 document.getElementById("btn").addEventListener("click", verify);
 
 function verify() {
-    var email = document.getElementById("email"); 
-    var password = document.getElementById("password"); 
+    var email = document.getElementById("email").value; 
+    var password = document.getElementById("password").value; 
     
     if (email === validEmail && password === validPassword) {
         alert("Login Successful!");
